dbaef35e4403f47c097dc45c90feb92468f6a449
Jandro-Creator <caespinoza12@espe.edu.ec>
2026-04-10 09:46:51 -0500

    Version4 - Ok

--- EjercicioPRUEBA.c
+++ EjercicioPRUEBA.c
@@ -3,7 +3,7 @@
 int main(){
     float num1;
     float num2;
-
+      printf("SUMA DE ENTEROS PARA REALIZAR UN PROMEDIO \n");
     while (num1 <= 0 || num2 <= 0){
     printf("Ingrese la nota 1: \n"); scanf("%f", &num1);
     printf("Ingrese la nota 2: \n"); scanf("%f", &num2);
@@ -12,6 +12,8 @@ int main(){
     if (num1 >= 0 && num2 >= 0){
         float prom = (num1 + num2) / 2;
         printf("Su promedio es %.2f\n", prom);
+        float multi= num1 * num2;
+        printf("Su multiplicacion es %.2f\n", multi);
 
     if(prom >= 14){
         printf("Aprueba");
